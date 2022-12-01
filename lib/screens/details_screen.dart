016535aa34a85a7756d27850ddbb2c37b6ca5a80
EdgarG15 <edgarjhdez1501@gmail.com>
2022-12-01 08:21:36 -0600

    commit

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,11 +5,121 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? '';
+
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Hola'),
+      body: CustomScrollView(
+        slivers: [
+          const _CustomAppbar(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const _PosterAndTitle(),
+                const _OverView(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppbar extends StatelessWidget {
+  const _CustomAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverAppBar(
+      backgroundColor: Colors.red,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        title: Text('Movie'),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/loading.gif'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              fit: BoxFit.cover,
+              height: 200,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Titulo',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'Titulo Original',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.amber,
+                  ),
+                  const SizedBox(width: 5),
+                  Text(
+                    'movie.voteAverate',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _OverView extends StatelessWidget {
+  const _OverView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: const Text(
+        'Ullamco magna sit consequat deserunt eu consequat nulla. Laborum laboris nostrud et fugiat labore Lorem non eu ullamco excepteur consequat mollit ullamco. Et velit nisi id incididunt reprehenderit ullamco dolor cupidatat eiusmod ad mollit mollit. Consequat dolore exercitation deserunt sunt amet cupidatat do cupidatat. Pariatur aliqua aute velit magna minim exercitation id eu laboris labore dolor sit.',
+        textAlign: TextAlign.justify,
       ),
     );
   }
